e7a940a0548ce66e41a942ceb58a98a5672b6cb9
alex171215 <alexandercarrion918@gmail.com>
2026-09-23 10:53:36 -0500

    chore: configurar puerto dinámico para Render

--- nestjs-productos-grpc/src/main.ts
+++ nestjs-productos-grpc/src/main.ts
@@ -6,10 +6,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.GRPC,
+    // En main.ts, modifica el apartado options:
     options: {
       package: 'productos',
       protoPath: join(__dirname, 'productos.proto'),
-      url: '0.0.0.0:5000',
+      // Agrega esto: process.env.PORT || '5000'
+      url: `0.0.0.0:${process.env.PORT || 5000}`,
     },
   });
   await app.listen();
