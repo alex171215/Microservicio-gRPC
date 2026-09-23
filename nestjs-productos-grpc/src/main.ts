3844e6918002918326a2352dd16f8f59e10f3311
alex171215 <alexandercarrion918@gmail.com>
2026-09-23 11:03:48 -0500

    style: limpiar comentarios y dinamizar log de puerto en main.ts

--- nestjs-productos-grpc/src/main.ts
+++ nestjs-productos-grpc/src/main.ts
@@ -4,17 +4,16 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 5000;
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.GRPC,
-    // En main.ts, modifica el apartado options:
     options: {
       package: 'productos',
       protoPath: join(__dirname, 'productos.proto'),
-      // Agrega esto: process.env.PORT || '5000'
-      url: `0.0.0.0:${process.env.PORT || 5000}`,
+      url: `0.0.0.0:${port}`,
     },
   });
   await app.listen();
-  console.log('Microservicio gRPC escuchando en 0.0.0.0:5000');
+  console.log(`Microservicio gRPC escuchando en 0.0.0.0:${port}`);
 }
 bootstrap();
